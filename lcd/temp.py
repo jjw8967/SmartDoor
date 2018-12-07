bf1315bd16c4e1879862317dceadfc7eaadb77ca
sumin6298 <sumin6206@naver.com>
2018-12-07 18:55:09 +0900

    lcd complete

--- lcd/temp.py
+++ lcd/temp.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*-coding: utf-8 -*-
 import sys
 import os
 import request
@@ -20,16 +20,15 @@ soup = bs4.BeautifulSoup(html,'html5lib')
 temperature = soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text
 weather = soup.find('ul', class_='info_list').find('p', class_='cast_txt').text
 weather = weather.split(',')
-message2 = ''
 # print(weather[0])
 # weather_list = list(weather)
 # weather_list = weather_list.split(',')
 # print(weather_list)
 #
 # print(weather)
-print(location)
-print(temperature)
-print(weather[0])
+#print(location)
+#print(temperature)
+#print(weather[0])
 if weather[0] == '맑음' :
     weather[0] = 'sunny'
 elif weather[0] == '구름많음':
@@ -42,24 +41,12 @@ elif weather[0] == '안개':
     weather[0] = 'foggy'
 elif weather[0]== '비':
     weather[0] = 'rain'
-    message2 = "'Check Umbrella!'"
 elif weather[0] == '소나기':
     weather[0] = 'short_rain'
-    message2 = "'Check Umbrella!'"
-elif weather[0] == '눈':
-    weather[0] = 'Snow'
-    message2 = "'Outside SnowDay'"
-
 else :
-    weather[0] = "'Check Phone'"
-
-if int(temperature) < -5:
-    message2 = "'Very Very Cold!'"
-elif int(temperature) > 30:
-    message2 = "'Very Very Hot!'"
+    weather[0] = 'check_phone'
                                
 
 message = 'seoul:'+temperature+':'+weather[0]
-
-os.system("./lcdtest "+message+" "+message2)
-
+print(message)
+#os.system("./main "+message)
